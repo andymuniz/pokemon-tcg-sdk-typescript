f138721a50c09c90f34f2a2af8a5a6ba4ca711db
Andy Muniz <munizandy94@gmail.com>
2022-05-27 03:33:11 -0500

    Bump package version

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { PokemonTcgApiClient } from "../lib/pokemonTcgClient";
 
 const client = new PokemonTcgApiClient();
-client.setApiKey("62c12cea-f2fd-48c5-af98-e9a03b94af4d");
+client.setApiKey(import.meta.env.VITE_POKEMON_API_KEY);
 
 window.Pokemon = client;
