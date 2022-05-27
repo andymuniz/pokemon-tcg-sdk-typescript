838235fc282e52970f76dbebd7249e63f80c59c6
Andy Muniz <munizandy94@gmail.com>
2022-05-27 17:58:44 -0500

    Add react-query devtools

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -1,9 +1,11 @@
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function AppProviders({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={new QueryClient()}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
